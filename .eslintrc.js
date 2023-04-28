f22e41759a7f3639ce8982733559481043b95fe2
Nilanjan Sengupta <nilanjansengupta471@gmail.com>
2023-04-29 04:26:25 +0530

    first-commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    semi: 'warn',
+    quotes: ['warn', 'single'],
+    // Add more rules here as needed
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { createRoot } from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(<App />);
